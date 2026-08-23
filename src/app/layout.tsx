2f129d67f3e767a82ba6b13c7177dd738f5531fe
Furqan Shahbaz <luxestore@gmail.com>
2026-08-23 20:37:53 +0500

    Update browser tab title icon (favicon/apple-icon) with new neon shopping cart logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,6 +109,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/logo.png" sizes="any" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="shortcut icon" href="/logo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <meta name="theme-color" content="#0f172a" />
